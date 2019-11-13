0a59874a52418a715f7a0572531bd5764637eb48
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-11-14 01:01:43 +0900

    web socket connects through several browsers

--- main.go
+++ main.go
@@ -18,11 +18,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	_ = t.templ.Execute(w, nil)
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("listenAndServe:", err)
 	}
