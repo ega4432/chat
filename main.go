2a46fb0e37f52acaf0d95d2da98a9fe7ec6d3b77
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-11-16 22:51:56 +0900

    Pass the server addr in commnad line argument

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,14 +19,17 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	_ = t.templ.Execute(w, nil)
+	_ = t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
+	log.Println("Webサーバを開始します。ポート：", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("listenAndServe:", err)
 	}
