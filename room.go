64afdb23954286933b82ae35fa9cb8204f54b816
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-11-17 12:42:11 +0900

    Introduce log package

--- room.go
+++ room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/yoshimitsuEgashira/trace"
 )
 
 type room struct {
@@ -12,6 +13,7 @@ type room struct {
 	join    chan *client
 	leave   chan *client
 	clients map[*client]bool
+	tracer  trace.Tracer
 }
 
 func newRoom() *room {
@@ -28,16 +30,21 @@ func (r *room) run() {
 		select {
 		case client := <-r.join:
 			r.clients[client] = true
+			r.tracer.Trace("New client entered.")
 		case client := <-r.leave:
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("client is leaved.")
 		case msg := <-r.forward:
+			r.tracer.Trace("message received.")
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
+					r.tracer.Trace(" --  The message has been send to client.")
 				default:
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- Send message failed. Clean up clients.")
 				}
 			}
 		}
